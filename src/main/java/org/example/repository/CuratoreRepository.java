32931464ce48923bb7534b7c0f760ba5b2744bb3
HasTeriZ <yangweijei@gmail.com>
2025-08-28 13:43:54 +0200

    aggiunta repository per il modello Curatore

---
+++ src/main/java/org/example/repository/CuratoreRepository.java
@@ -0,0 +1,13 @@
+package org.example.repository;
+
+import org.example.model.Curatore;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CuratoreRepository extends MongoRepository<Curatore, String> {
+    Optional<Curatore> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
